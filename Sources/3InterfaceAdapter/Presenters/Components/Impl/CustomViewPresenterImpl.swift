64b2a07fab89884fb4767125316841c9009bff5c
Comparini <alecomparini.dev@gmail.com>
2023-09-06 15:59:15 -0300

    create custom view

---
+++ Sources/3InterfaceAdapter/Presenters/Components/Impl/CustomViewPresenterImpl.swift
@@ -0,0 +1,46 @@
+//  Created by Alessandro Comparini on 04/09/23.
+//
+
+import Foundation
+import CustomComponentsSDK
+import DSMUseCase
+
+public class CustomViewPresenterImpl: ComponentPresenter {
+    
+    private let getComponentUseCase: GetComponentUseCase
+    
+    private let customView: ViewBuilder
+    
+    public init(getComponentUseCase: GetComponentUseCase, customView: ViewBuilder) {
+        self.getComponentUseCase = getComponentUseCase
+        self.customView = customView
+    }
+    
+    
+//  MARK: - PUBLIC AREA
+    
+    public func get(id: Int) {
+        let dto: GetComponentUseCaseDTO.Output? = getComponentUseCase.get(id: id)
+        configComponent(dto)
+    }
+    
+    public func get(name: String) {
+        let dto: GetComponentUseCaseDTO.Output? = getComponentUseCase.get(name: name)
+        configComponent(dto)
+    }
+    
+    public func get(uuid: String) {
+        let dto: GetComponentUseCaseDTO.Output? = getComponentUseCase.get(uuid: uuid)
+        configComponent(dto)
+    }
+    
+    
+//  MARK: - PRIVATE AREA
+    private func configComponent(_ dto: GetComponentUseCaseDTO.Output?) {
+        guard let dto else { return }
+        self.customView
+            .setBackgroundColor(named: dto.backgroundColor)
+            .setBackgroundColor(hexColor: dto.backgroundColor)
+    }
+    
+}
---
+++ Sources/Main/Components/CustomView.swift
@@ -0,0 +1,32 @@
+//  Created by Alessandro Comparini on 29/08/23.
+//
+
+import UIKit
+
+import CustomComponentsSDK
+import DSMDomain
+import DSMPresenters
+import DSMUseCase
+
+
+open class CustomView: ViewBuilder {
+    private static let identifier = "customView"
+    
+    
+//  MARK: - INITIALIZERS
+    
+    public override init() {
+        super.init()
+        configure()
+    }
+
+    
+//  MARK: - PRIVATE AREA
+    private func configure() {
+        let cacheManager = CacheManager.shared
+        let getComponentUseCase = GetComponentUseCaseImpl(cacheManager: cacheManager)
+        let presenter = CustomViewPresenterImpl(getComponentUseCase: getComponentUseCase, customView: self)
+        presenter.get(name: CustomView.identifier)
+    }
+        
+}
