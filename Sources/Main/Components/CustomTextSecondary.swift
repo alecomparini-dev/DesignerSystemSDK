b4a87fb67e18016aa03ad34129edd75db7e03b90
Comparini <alecomparini.dev@gmail.com>
2023-09-13 08:59:21 -0300

    create customTextSecondary

---
+++ Sources/Main/Components/CustomTextSecondary.swift
@@ -0,0 +1,38 @@
+//  Created by Alessandro Comparini on 29/08/23.
+//
+
+import UIKit
+
+import CustomComponentsSDK
+import DSMPresenters
+import DSMUseCase
+import DSMUseCaseGateway
+import MemoryCache
+
+
+open class CustomTextSecondary: LabelBuilder {
+    private static let identifier = "customTextSecondary"
+    
+    
+//  MARK: - INITIALIZERS
+    
+    public override init() {
+        super.init()
+        configure()
+    }
+
+    
+//  MARK: - PRIVATE AREA
+    private func configure() {
+        let memoryCache = ComponentMemoryCache.shared
+        
+        let getComponentUseCaseGateway = MemoryGetComponentUseCaseGatewayImpl(memoryCache: memoryCache)
+        
+        let getComponentUseCase = GetComponentUseCaseImpl(getComponentUseCaseGateway: getComponentUseCaseGateway)
+        
+        let presenter = CustomTextSecondaryPresenterImpl(getComponentUseCase: getComponentUseCase, customTextSecondary: self)
+        
+        presenter.get(name: CustomTextSecondary.identifier)
+    }
+        
+}
